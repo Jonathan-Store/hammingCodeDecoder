bd199fea584d2c791736cfc6f23833190af1b63e
Jonathan-Store <147146518+Jonathan-Store@users.noreply.github.com>
2023-10-17 12:04:41 +0100

    Update hamingCodeDecoder.py

--- hamingCodeDecoder.py
+++ hamingCodeDecoder.py
@@ -1,5 +1,37 @@
 #decode hamming codes
 
+def locateError(array,checkC13,checkC23,checkR13,checkR23):
+    
+    if checkC13 == False and checkC23 == False:
+        errorLocationC = 0
+    elif checkC13 == True and checkC23 == False:
+        errorLocationC = 1
+    elif checkC13 == False and checkC23 == True:
+        errorLocationC = 2
+    else:
+        errorLocationC = 3
+    
+    
+    if checkR13 == False and checkC23 == False:
+        errorLocationR = 0
+    elif checkR13 == True and checkC23 == False:
+        errorLocationR = 1
+    elif checkR13 == False and checkC23 == True:
+        errorLocationR = 2
+    else:
+        errorLocaitonR = 3
+    
+    if array[errorLocationR,errorLocationC] == 1:
+        array[errorLocationR,errorLocationC] = 0
+    else:
+        array[errorLocationR,errorLocationC] = 1
+    
+    return(array)
+    
+    
+    
+    
+
 def arrayErrorCheck(array):
     total1s = 0
     for i in array:
@@ -11,11 +43,12 @@ def arrayErrorCheck(array):
             if i == 1:
                 total1s = total1s+1
     if total1s % 2 == 0: #Has no errors if %2 = 0, ∴ return false
+    
         return False
     else:
         return True
 
-def checkColumn13(array):
+def funCheckColumn13(array):
     array1 = []
     array3 = []
     
@@ -25,8 +58,53 @@ def checkColumn13(array):
     array1Check = arrayErrorCheck(array1)
     array3Check = arrayErrorCheck(array3)
     
-    return(array1Check, array3Check)
+    if array1Check == False and array3Check == False:
+        return(False)
+    else:
+        return(True)
+    
 
+def funCheckColumn23(array):
+    array2 = []
+    array3 = []
+    
+    for i in array:
+        array2.append(i[2])
+        array3.append(i[3])
+    array2Check = arrayErrorCheck(array2)
+    array3Check = arrayErrorCheck(array3)
+    
+    if array2Check == False and array3Check == False:
+        return(False)
+    else:
+        return(True)
+    
+
+def funCheckRow13(array):
+    array1 = array[1]
+    array2 = array[3]
+    
+    array1Check = arrayErrorCheck(array1)
+    array2Check = arrayErrorCheck(array2)
+    
+    if array1Check == False and array2Check == False:
+        return(False)
+    else:
+        return(True)
+
+def funCheckRow23(array):
+    array2 = array[2]
+    array3 = array[3]
+    
+    array2Check = arrayErrorCheck(array2)
+    array3Check = arrayErrorCheck(array3)
+    
+    if array2Check == False and array3Check == False:
+        return(False)
+    else:
+        return(True)
+    
+    
 def main():
 
     array = [
@@ -36,9 +114,17 @@ def main():
         [0,0,0,0]
         ]
     hasErrors = arrayErrorCheck(array)
-    print(hasErrors)
+    print("array:",hasErrors)
     
-    check13 = checkColumn13(array)
-    print(check13)
+    checkColumn13 = funCheckColumn13(array)
+    print("col13:",checkColumn13)
+    checkColumn23 = funCheckColumn23(array)
+    print("col23:",checkColumn23)
+    checkRow13 = funCheckRow13(array)
+    print("row13:",checkRow13)
+    checkRow23 = funCheckRow23(array)
+    print("row23:",checkRow23)
+
+    fixedArray = locateError(array,checkColumn13,checkColumn23,checkRow13,checkRow23)
     
 main()
